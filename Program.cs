8a4e4c4a98d2723990d669a82fd0a26984e064bd
AnatoliyIB <153524711+AnatoliyIB@users.noreply.github.com>
2024-02-18 19:23:46 +0300

    WorkingRelease1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+//длина которых меньше, либо равна 3 символам.
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+int MaxLength = 3;
+Console.WriteLine("Enter strings separated by ESPACE for array:");
+string input = Console.ReadLine()!;
+string[] array = input.Split(' ');
+
+int CountOfShortElements(string[] array)
+{
+    int count = 0;
+
+    foreach (string s in array)
+    {
+        if (s.Length <= MaxLength)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] CreateShortElementsArray(string[] array, int NewArrayLength)
+{
+    string[] newArray = new string[NewArrayLength];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= MaxLength)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+
+    return newArray;
+}
+
+void PrintStringArray(string[] array)
+{
+    foreach (string s in array)
+    {
+        Console.Write(s + " ");
+    }
+}
+
+PrintStringArray(CreateShortElementsArray(array, CountOfShortElements(array)));
